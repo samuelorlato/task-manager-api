bb916cc148dab36b18b9527f08c4f62261acb6c5
Samuel Orlato <samuelorlato@gmail.com>
2023-12-13 00:51:00 -0300

    added method to get all tasks on repository port [feat]

--- internal/core/ports/task_repository_port.go
+++ internal/core/ports/task_repository_port.go
@@ -3,6 +3,7 @@ package ports
 import "github.com/samuelorlato/task-manager-api/internal/core/models"
 
 type TaskRepository interface {
+	GetTasks() []*models.Task
 	CreateTask(title string, description *string, toDate string) error
 	GetTask(taskId string) (*models.Task, error)
 	UpdateTask(taskId string, title *string, description *string, toDate *string, completed *bool) error
